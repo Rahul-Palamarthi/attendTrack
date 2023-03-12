04b04bd0868d69c4f9c5ff6722a857a3d2f770f8
Rahul Palamarthi <rahulpalamarthi@gmail.com>
2023-03-12 13:11:54 +0530

    added Data dir

---
+++ src/Data/colors.js
@@ -0,0 +1,22 @@
+const colors = [
+    "#aa00ff",
+    "#80d8fe",
+    "#cdff90",
+    "#ea80fc",
+    "#83ffff",
+    "#651efe",
+    "#a7feeb",
+    "#304fff",
+    "#ff8a80",
+    "#ff5353",
+    "#ff80ab",
+    "#ff4181",
+    "#b388fe",
+    "#ffff00",
+    "#ffd181",
+];
+const grey = "hsl(0, 0%, 10%)";
+
+export default colors;
+
+export { grey };
